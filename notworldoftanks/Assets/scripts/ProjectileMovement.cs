dc4dba47e81671dff80f38cdb9b30f65895be0b6
David Ewald <ewalddav@gmail.com>
2019-03-17 13:08:07 -0700

    OMFG it works! things shoot out of cannon properly, along the correct axis, with gravity

--- notworldoftanks/Assets/scripts/ProjectileMovement.cs
+++ notworldoftanks/Assets/scripts/ProjectileMovement.cs
@@ -2,11 +2,11 @@
 
 public class ProjectileMovement : MonoBehaviour
 {
-    public float thrust = 100.0f;
+    public float thrust = 50.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(transform.forward * thrust, ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(transform.up * thrust, ForceMode.Impulse);
     }
 }
