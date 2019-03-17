407e36883aea567dbf1f4587cb42ac2836c0c062
David Ewald <ewalddav@gmail.com>
2019-03-17 13:17:25 -0700

    edits and thangs, work getting done homie

--- notworldoftanks/Assets/scripts/ProjectileMovement.cs
+++ notworldoftanks/Assets/scripts/ProjectileMovement.cs
@@ -2,9 +2,8 @@
 
 public class ProjectileMovement : MonoBehaviour
 {
-    public float thrust = 50.0f;
+    public float thrust = 15.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().AddForce(transform.up * thrust, ForceMode.Impulse);
